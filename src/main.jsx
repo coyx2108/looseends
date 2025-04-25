32d8cb7ad6ef2f57af38adc8c81717f32953ef8a
coyx2108 <chantelong@u.nus.edu>
2025-04-25 14:48:31 +0800

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename='/looseends'>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
